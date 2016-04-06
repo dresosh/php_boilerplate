1bc7a2e7b28e33384d67d5a8ef6930a75e4987af
dresosh <contact@dresosh.com>
2016-04-06 14:40:58 -0700

    adds bower bootstrap jquery modernizr

--- prod/_common/head.php
+++ prod/_common/head.php
@@ -3,6 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>Test</title>
-    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/main.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="./bower_components/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="./bower_components/bootstrap/dist/css/bootstrap-theme.min.css" media="screen" title="no title" charset="utf-8">
+    <script src="./js/modernizr-2.8.3-respond-1.4.2.min.js"></script>
   </head>
   <body>
