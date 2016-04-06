8f0d7e5090c92f26ba0623f3c439510565ca9a91
dresosh <contact@dresosh.com>
2016-04-06 15:11:15 -0700

    adds new navbar, favicon, human.txt, robots.txt apple-touch-icon 404.html

--- prod/_common/head.php
+++ prod/_common/head.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="icon" href="./images/favicon.ico" media="screen" title="no title" charset="utf-8">
     <title>Test</title>
     <link rel="stylesheet" href="./css/main.css" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="./bower_components/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
