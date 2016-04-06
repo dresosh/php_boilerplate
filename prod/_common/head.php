20440e32e2cb9416ea41b7c5eefdb2b1ae2458b3
dresosh <contact@dresosh.com>
2016-04-06 15:45:33 -0700

    creating new nav and making it responsive

--- prod/_common/head.php
+++ prod/_common/head.php
@@ -2,8 +2,10 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="icon" href="./images/favicon.ico" media="screen" title="no title" charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Test</title>
+    <link rel="icon" href="./images/favicon.ico" media="screen" title="no title" charset="utf-8">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <link rel="stylesheet" href="./css/main.css" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="./bower_components/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="./bower_components/bootstrap/dist/css/bootstrap-theme.min.css" media="screen" title="no title" charset="utf-8">
