759b7d07e00d6651386a984c07ce8b5a3ede5cb9
dresosh <contact@dresosh.com>
2016-06-01 12:35:27 -0700

    moves jquery script to head, changes js link from main.js to main.min.js

--- prod/_common/footer.php
+++ prod/_common/footer.php
@@ -1,5 +1,4 @@
-<script src='./js/main.js' type="text/javascript"></script>
-<script src='./bower_components/jquery/dist/jquery.min.js' type="text/javascript"></script>
+<script src='./js/main.min.js' type="text/javascript"></script>
 <script src='./bower_components/bootstrap/dist/js/bootstrap.min.js' type="text/javascript"></script>
 </body>
 </html>
--- prod/_common/head.php
+++ prod/_common/head.php
@@ -10,5 +10,6 @@
     <link rel="stylesheet" href="./bower_components/bootstrap/dist/css/bootstrap-theme.min.css" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="./css/main.css" media="screen" title="no title" charset="utf-8">
     <script src="./js/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <script src='./bower_components/jquery/dist/jquery.min.js' type="text/javascript"></script>
   </head>
   <body>
