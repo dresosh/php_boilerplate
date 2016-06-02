56c53e2a959ac150e49661b4513a48e9c8f3883e
dresosh <contact@dresosh.com>
2016-06-01 19:27:49 -0700

    old code

--- prod/_common/head.php
+++ prod/_common/head.php
@@ -6,8 +6,6 @@
     <title>Test</title>
     <link rel="icon" href="./images/favicon.ico" media="screen" title="no title" charset="utf-8">
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
-    <link rel="stylesheet" href="./bower_components/bootstrap/dist/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
-    <link rel="stylesheet" href="./bower_components/bootstrap/dist/css/bootstrap-theme.min.css" media="screen" title="no title" charset="utf-8">
     <link rel="stylesheet" href="./css/main.css" media="screen" title="no title" charset="utf-8">
     <script src="./js/modernizr-2.8.3-respond-1.4.2.min.js"></script>
   </head>
